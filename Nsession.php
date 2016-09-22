cdc46c9f4b8cb6b4d7af6199bc66105f095d48a5
Jian Liu <onanying@126.com>
2016-09-22 17:38:11 +0800

    初次开源

---
+++ Nsession.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * codeigniter使用php原生session
+ * @author 刘健 <59208859@qq.com>
+ * 完全兼容codeigniter 3，本session类除了Flashdata/Tempdata外的其他都有按codeigniter的方法重写为原生
+ */
+class Nsession
+{
+
+    public function __construct()
+    {
+        session_start();
+    }
+
+    // 获取 Session 数据
+    public function __get($name)
+    {
+        return isset($_SESSION[$name]) ? $_SESSION[$name] : null;
+    }
+
+    // 添加 Session 数据
+    public function __set($name, $value)
+    {
+        $_SESSION[$name] = $value;
+    }
+
+    // 获取 Session 数据 (为了和之前的版本兼容)
+    public function userdata($name)
+    {
+        return $_SESSION[$name];
+    }
+
+    // 添加 Session 数据 (为了和之前的版本兼容)
+    public function set_userdata($args1, $args2 = null)
+    {
+        switch (func_num_args()) {
+            case 1:
+                foreach ($args1 as $key => $value) {
+                    $this->__set($key, $value);
+                }
+                break;
+            case 2:
+                $this->__set($args1, $args2);
+                break;
+        }
+    }
+
+    // 删除 Session 数据
+    public function unset_userdata($args1)
+    {
+        if (is_array($args1)) {
+            foreach ($args1 as $name) {
+                unset($_SESSION[$name]);
+            }
+        } else {
+            unset($_SESSION[$args1]);
+        }
+    }
+
+    // 销毁 Session
+    public function sess_destroy()
+    {
+        session_destroy();
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# CodeIgniter-PHPSession
+## 简介
+
 CodeIgniter 的 session 类因为无法做到关闭浏览器session就失效，对于开发后台管理有安全隐患；如果强行配置$config['sess_expiration'] = 0;  则会导致服务器session文件不会被清理的问题，既然PHP原生的session本来就很好用，在不改变项目代码的情况下，所以我按CodeIgniter session类的方法重写了一个使用原生php session的类，可无缝切换。
+
+### 使用 Session 类
+
+和CodeIgniter的session类的方法一模一样，只是载入的类由session变为nsession，[官方文档](http://codeigniter.org.cn/user_guide/libraries/sessions.html)。
+
+#### 初始化 Session 类
+
+```
+$this->load->library('nsession');
+```
+
+#### 获取 Session 数据
+
+```php
+// 获取单个键值
+$_SESSION['item']
+$this->nsession->item
+$this->nsession->userdata('item');
+```
+
+```php
+// 获取所有键值
+$_SESSION
+$this->nsession->userdata();
+```
+
+#### 添加 Session 数据
+
+```php
+// 添加多个值
+$newdata = array(
+    'username'  => 'johndoe',
+    'email'     => 'johndoe@some-site.com',
+    'logged_in' => TRUE
+);
+$this->nsession->set_userdata($newdata);
+// 添加一个值
+$this->nsession->set_userdata('some_name', 'some_value');
+```
+
+#### 检查某个 session 值是否存在
+
+```php
+isset($_SESSION['some_name'])
+$this->nsession->has_userdata('some_name');
+```
+
+#### 删除 Session 数据
+
+
+```php
+unset($_SESSION['some_name']);
+
+$this->nsession->unset_userdata('some_name');
+
+$array_items = array('username', 'email');
+$this->nsession->unset_userdata($array_items);
+```
